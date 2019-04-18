a78ec576d44a220d82ac9e77aeae3653c47544e0
Wendyhartford <33272230+Wendyhartford@users.noreply.github.com>
2019-04-18 11:04:05 -0600

    Map works!
    With dark styling

--- MapController.js
+++ MapController.js
@@ -2,7 +2,7 @@
 var map;
 
 function initMap(){
-    var pos = {lat: -116.2157089, lng: 43.6060238};
+    var pos = {lat: 43.6016653, lng: -116.2106962};
     // The map, centered at Uluru
     var map = new google.maps.Map(
         document.getElementById('map'), {zoom: 10, center: pos});
