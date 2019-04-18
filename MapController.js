f1bb97951eb5722006d790c5f2a7eb2654bf2531
Wendyhartford <33272230+Wendyhartford@users.noreply.github.com>
2019-04-18 10:23:09 -0600

    Progress
    Still nothing works though

--- MapController.js
+++ MapController.js
@@ -1,1 +1,11 @@
 // JavaScript source code
+var map;
+
+function initMap(){
+    var pos = {lat: -116.2157089, lng: 43.6060238};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 10, center: pos});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: pos, map: map});
+}
