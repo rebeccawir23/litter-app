ac3519b7f5ce20b9e89314b6edf8640eb1fabc5b
Wendyhartford <33272230+Wendyhartford@users.noreply.github.com>
2019-04-25 13:51:46 -0600

    ionic work

--- MapController.js
+++ MapController.js
@@ -5,7 +5,7 @@ function initMap(){
     var pos = {lat: 43.6016653, lng: -116.2106962};
     // The map, centered at Uluru
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 10, center: pos});
+        document.getElementById('map'), {zoom: 2, center: pos});
     // The marker, positioned at Uluru
     var marker = new google.maps.Marker({position: pos, map: map});
 }
